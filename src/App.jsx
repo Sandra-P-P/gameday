344332d9ccc75548bdc86333b44d19ff92bcdbee
Sandra-P-P <chikkumav1@gmail.com>
2024-09-03 01:21:43 +0530

    init victoryboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { Button } from "@salt-ds/core";
-import "./App.css";
+import VictoryBoard from "./components/VictoryBoard/VictoryBoard";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const handleRequestOpen = () => {
+    setOpen(true);
+  };
+
+  const onOpenChange = (value) => {
+    setOpen(value);
+  };
   return (
-    <div>
-      <Button variant="cta"> Click me</Button>
-    </div>
+    <>
+      <div className={styles.App}>
+        <h1>Heading</h1>
+        <Button variant="cta" onClick={handleRequestOpen}>
+          {" "}
+          Click me
+        </Button>
+        <VictoryBoard open={open} onOpenChange={onOpenChange}></VictoryBoard>
+      </div>
+    </>
   );
 }
 
